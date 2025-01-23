b7e6d21d61061337997db7689e733aa336ad049d
“23jn0333” <23jn0333@jec.ac.jp>
2025-01-23 14:25:21 +0900

    “DB接続文字列を732実習室からAzureSQLデータベースへ変更”

--- helpers/config.php
+++ helpers/config.php
@@ -2,4 +2,4 @@
     // DB接続設定
     define('DSN', 'sqlsrv:server = tcp:23jn0333db.database.windows.net,1433; Database = 23jn0333DB", "jndb", "Pa$$word1234');
     define('DB_USER', '23jn0333');
-    define('DB_PASSWORD', '23jn0333');
+    define('DB_PASSWORD', 'Pa$$word1234');
