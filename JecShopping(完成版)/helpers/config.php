c96c158f3c4bde49add08a9b3b8c1eb63759eb25
“23jn0333” <23jn0333@jec.ac.jp>
2025-01-23 13:59:12 +0900

    “DB接続文字列を732実習室からAzureSQLデータベースへ変更”

--- JecShopping(完成版)/helpers/config.php
+++ JecShopping(完成版)/helpers/config.php
@@ -1,5 +1,5 @@
 <?php
     // DB接続設定
-    define('DSN', 'sqlsrv:server=10.32.97.1;database=20jn0300');
-    define('DB_USER', '20jn0300');
-    define('DB_PASSWORD', '20jn0300');
+    define('DSN', 'sqlsrv:server = tcp:23jn0333db.database.windows.net,1433; Database = 23jn0333DB", "jndb", "{Pa$$word1234}');
+    define('DB_USER', '23jn0333');
+    define('DB_PASSWORD', '23jn0333');
